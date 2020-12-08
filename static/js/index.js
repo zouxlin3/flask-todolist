e3409fc2c44bd1da05f975bc1c6dfa1421a88f94
zouxlin3 <944438083@qq.com>
2020-12-09 00:02:37 +0800

    v0.42 任务列表

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,18 @@
 function add() {
     var post = document.createElement("form");
-    var task = document.getElementById("addtask");
+    var task = document.getElementById("addtask").value;
     var func = "add";
 
     post.style.display = "none";
-    post.methods = "post";
-    post.setAttribute("contnet", task);
-    post.setAttribute("func", func);
+    post.method = "POST";
+    var post_task = document.createElement("input");
+    var post_func = document.createElement("input");
+    post_task.name = "content";
+    post_task.value = task;
+    post_func.name = "func";
+    post_func.value = func;
+    post.appendChild(post_task);
+    post.appendChild(post_func);
 
     document.body.appendChild(post);
 
